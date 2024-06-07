646b0acffae00e4f316f695fcdf892905e7de0f1
Rachana Patil <rachanapatil@Rachanas-MacBook-Pro.local>
2024-06-07 14:31:48 -0700

    context, Home and Notfound component

--- src/App.js
+++ src/App.js
@@ -1,49 +1,37 @@
-import React, { Fragment, useState } from 'react';
+import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
+
 import User from './components/users/User';
-import Search from './components/users/Search';
+import Home from './components/pages/Home';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 
 import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
 //import Spinner from './components/layout/spinner';
 //import spinner from './components/layout/spinner.gif';
 const App = () => {
-  const [alert, setAlert] = useState(null);
-
-  //set Alert
-  const showAlert = (msg, type) => {
-    setAlert({ msg, type });
-    setTimeout(() => setAlert(null), 5000);
-  };
-
   return (
     <GithubState>
-      <Router>
-        <div className="App">
-          <Navbar />
-          <div className="container">
-            <Alert alert={alert} />
-            <Routes>
-              <Route
-                exact
-                path="/"
-                element={
-                  <Fragment>
-                    <Search setAlert={showAlert} />
-                    <Users />
-                  </Fragment>
-                }
-              />
-              <Route path="/about" element={<About />} />
-              <Route path="/user/:login" element={<User />} />
-            </Routes>
+      <AlertState>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <div className="container">
+              <Alert />
+              <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/user/:login" element={<User />} />
+                <Route path="/*" element={<NotFound />} />
+              </Routes>
+            </div>
           </div>
-        </div>
-      </Router>
+        </Router>
+      </AlertState>
     </GithubState>
   );
 };
